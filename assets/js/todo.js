10408fc5ae6c8f83b2e3f95619a440755b1be7bc
suikim <susaninhakim@gmail.com>
2019-09-01 16:54:59 -0700

    add-task-func
    Added function to add new tasks to todoList.
    Changed functions to fade out and delete tasks to apply events to future tasks added.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,16 +1,32 @@
 //Function to fade out task
-$("li").on("click", function(){
+//$("ul").on("click", "li", function() -- adds event to every li within ul
+//$("li").on("click", function() -- same as $("li").click()
+
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("done");
 });
 
 //Delete function
-$("span").on("click", function(){
+$("ul").on("click", "span", function(){
 	$(this).parent().fadeOut(400, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
+//Add task function
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		//if val not empty
+		if($(this).val() != ""){
+			//add li to ul
+			$("ul").append('<li><span class="delete">X </span>'+ $(this).val()+ '</li>')
+			//empty val after adding task
+			$(this).val("");
+		}
+		
+	} 	
+});
 
 // $("#addTask").click(function(){
 // 	$("ul").add("li", "hello");
