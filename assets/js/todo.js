82fe17a2e84a6bf26383b2a1c035bf690920b030
suikim <susaninhakim@gmail.com>
2019-09-01 02:26:03 -0700

    delete-and-fade-function
    Added functionality to fade out task items and 'X's fade and delete li in ul

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,17 @@
-$()
+//Function to fade out task
+$("li").on("click", function(){
+	$(this).toggleClass("done");
+});
+
+//Delete function
+$("span").on("click", function(){
+	$(this).parent().fadeOut(400, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+
+// $("#addTask").click(function(){
+// 	$("ul").add("li", "hello");
+// });
